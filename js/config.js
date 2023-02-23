0a88c4ad4f0052c5104a7c9d2a76dda4c0d0f293
KPalasti <89475786+KPalasti@users.noreply.github.com>
2023-02-23 11:45:32 -0700

    user pool integration, still lacking api invokeurl

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_kHaqNSknR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6pv52s9htalgcd1r4kqc6r6pes', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_qpxcynUuT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6hfblusopv834f0dtbfgduamoc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
