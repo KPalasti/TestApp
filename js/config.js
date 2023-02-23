303eb2eb662a8018906a3e025f99f01c6bcd5b9b
KPalasti <89475786+KPalasti@users.noreply.github.com>
2023-02-23 11:13:25 -0700

    changed entry modal, login, removed widget options

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_kHaqNSknR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6pv52s9htalgcd1r4kqc6r6pes', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://kua8fml5sg.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
