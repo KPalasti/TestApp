ba6369f2c904edd060ab63c87a263aca1986e360
KPalasti <89475786+KPalasti@users.noreply.github.com>
2023-02-23 13:37:23 -0700

    changed user pool and userpool id

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_qpxcynUuT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6hfblusopv834f0dtbfgduamoc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_rI2dQX0KB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '54bt1ivi9eraliholtegbnu5hq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://kua8fml5sg.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
